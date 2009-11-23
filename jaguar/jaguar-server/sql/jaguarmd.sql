06384a9ff400290719a4ec0f9afe9ca1f96aea3b
Cross <cross@dev-juanc.(none)>
2009-11-23 13:50:01 -0500

    Task Definition load

--- jaguar/jaguar-server/sql/jaguarmd.sql
+++ jaguar/jaguar-server/sql/jaguarmd.sql
@@ -1,7 +1,7 @@
 -- MySQL Administrator dump 1.4
 --
 -- ------------------------------------------------------
--- Server version	5.0.51a-3ubuntu5.1
+-- Server version	5.1.37-1ubuntu5
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -22,6 +22,31 @@ CREATE DATABASE IF NOT EXISTS jaguarmd;
 USE jaguarmd;
 
 --
+-- Definition of table `jaguarmd`.`activities`
+--
+
+DROP TABLE IF EXISTS `jaguarmd`.`activities`;
+CREATE TABLE  `jaguarmd`.`activities` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `activitytype` int(11) DEFAULT NULL,
+  `startquantity` int(11) NOT NULL DEFAULT '1',
+  `looptype` int(11) NOT NULL,
+  `idprocessdef` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jaguarmd`.`activities`
+--
+
+/*!40000 ALTER TABLE `activities` DISABLE KEYS */;
+LOCK TABLES `activities` WRITE;
+INSERT INTO `jaguarmd`.`activities` VALUES  (1,0,1,0,1);
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `activities` ENABLE KEYS */;
+
+
+--
 -- Definition of table `jaguarmd`.`processdef`
 --
 
@@ -31,7 +56,7 @@ CREATE TABLE  `jaguarmd`.`processdef` (
   `defname` varchar(100) NOT NULL,
   `processType` smallint(6) NOT NULL,
   `masterent` varchar(100) NOT NULL,
-  PRIMARY KEY  (`id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 COMMENT='Process Definition';
 
 --
@@ -48,6 +73,30 @@ INSERT INTO `jaguarmd`.`processdef` VALUES  (1,'def1',1,'temp'),
  (6,'def6',1,'temp');
 UNLOCK TABLES;
 /*!40000 ALTER TABLE `processdef` ENABLE KEYS */;
+
+
+--
+-- Definition of table `jaguarmd`.`task`
+--
+
+DROP TABLE IF EXISTS `jaguarmd`.`task`;
+CREATE TABLE  `jaguarmd`.`task` (
+  `id` int(11) NOT NULL DEFAULT '0',
+  `idactivity` int(11) DEFAULT NULL,
+  `taskname` varchar(20) DEFAULT NULL,
+  `tasktype` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `jaguarmd`.`task`
+--
+
+/*!40000 ALTER TABLE `task` DISABLE KEYS */;
+LOCK TABLES `task` WRITE;
+INSERT INTO `jaguarmd`.`task` VALUES  (1,1,'test',0);
+UNLOCK TABLES;
+/*!40000 ALTER TABLE `task` ENABLE KEYS */;
 
 
 
