08e8a8e97480497b1daa10e675c169dff10eb6b6
root <root@cross-ubuntu.(none)>
2010-04-11 21:32:50 -0500

    A sample screen Customers

---
+++ jaguar/jaguar-client/jclientgen/Clientes.cpp
@@ -0,0 +1,32 @@
+/*
+ * File:   Clientes.cpp
+ * Author: cross
+ *
+ * Created on February 24, 2010, 10:14 PM
+ */
+
+#include "Clientes.h"
+#include "ui/TextSimple.h"
+
+Clientes::Clientes(QWidget* parent, Qt::WindowFlags flags) : ScreenBase(parent, flags) {
+    TextSimple* s1 = new TextSimple();
+    s1->setEndLine(true);
+    addRow("Name", s1);
+    TextSimple* s2 = new TextSimple();
+    s2->setEndLine(true);
+    addRow("Last Name", s2);
+    TextSimple* s3 = new TextSimple();
+    s3->setEndLine(true);
+    addRow("Birth Date", s3);
+    TextSimple* s4 = new TextSimple();
+    s4->setEndLine(true);
+    addRow("Address", s4);
+    TextSimple* s5 = new TextSimple();
+    s5->setEndLine(true);
+    addRow("Phone Number", s5);
+
+    addAction("Action1");
+}
+
+Clientes::~Clientes() {
+}
--- jaguar/jaguar-client/jclientgen/base.cpp
+++ jaguar/jaguar-client/jclientgen/base.cpp
@@ -1,11 +1,18 @@
 #include "jaguarui.h"
 #include "Radicacion.h"
+#include "Clientes.h"
 
 ScreenBase* createRadicacion() {
     Radicacion* rad = new Radicacion();
     return rad;
 }
 
+ScreenBase* createClientes() {
+    Clientes* cl = new Clientes();
+    return cl;
+}
+
 void registerClient() {
     addScreen(1, &createRadicacion, "Radicacion");
+    addScreen(2, &createClientes, "Clientes");
 }
