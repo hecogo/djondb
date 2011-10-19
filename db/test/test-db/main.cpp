0720a109c62da98a8e7e7ffe3cbaecfaf02906ba
Cross <crossleyjuan@gmail.com>
2011-10-19 17:58:08 -0500

    logger modified

--- db/test/test-db/main.cpp
+++ db/test/test-db/main.cpp
@@ -23,114 +23,15 @@ DBController controller;
 
 std::vector<std::string*> __ids;
 
-#ifdef LINUX
-timespec diff(timespec start, timespec end)
-{
-	timespec temp;
-	if ((end.tv_nsec-start.tv_nsec)<0) {
-		temp.tv_sec = end.tv_sec-start.tv_sec-1;
-		temp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-	} else {
-		temp.tv_sec = end.tv_sec-start.tv_sec;
-		temp.tv_nsec = end.tv_nsec-start.tv_nsec;
-	}
-	return temp;
-}
-#endif
-
-#ifdef WINDOWS
-timeval diff(timeval start, timeval end)
-{
-	timeval temp;
-	if ((end.tv_usec-start.tv_usec)<0) {
-		temp.tv_sec = end.tv_sec-start.tv_sec-1;
-		temp.tv_usec = 1000000+end.tv_usec-start.tv_usec;
-	} else {
-		temp.tv_sec = end.tv_sec-start.tv_sec;
-		temp.tv_usec = end.tv_usec-start.tv_usec;
-	}
-	return temp;
-}
-#endif
-
 int testInsert(BSONObj* o) {
     controller.insert("sp1.customer", o);
 }
 
-#ifdef WINDOWS
-LARGE_INTEGER getFILETIMEoffset()
-{
-    SYSTEMTIME s;
-    FILETIME f;
-    LARGE_INTEGER t;
-
-    s.wYear = 1970;
-    s.wMonth = 1;
-    s.wDay = 1;
-    s.wHour = 0;
-    s.wMinute = 0;
-    s.wSecond = 0;
-    s.wMilliseconds = 0;
-    SystemTimeToFileTime(&s, &f);
-    t.QuadPart = f.dwHighDateTime;
-    t.QuadPart <<= 32;
-    t.QuadPart |= f.dwLowDateTime;
-    return (t);
-}
-
-int clock_gettime(int X, struct timeval *tv)
-{
-    LARGE_INTEGER           t;
-    FILETIME            f;
-    double                  microseconds;
-    static LARGE_INTEGER    offset;
-    static double           frequencyToMicroseconds;
-    static int              initialized = 0;
-    static BOOL             usePerformanceCounter = 0;
-
-    if (!initialized) {
-        LARGE_INTEGER performanceFrequency;
-        initialized = 1;
-        usePerformanceCounter = QueryPerformanceFrequency(&performanceFrequency);
-        if (usePerformanceCounter) {
-            QueryPerformanceCounter(&offset);
-            frequencyToMicroseconds = (double)performanceFrequency.QuadPart / 1000000.;
-        } else {
-            offset = getFILETIMEoffset();
-            frequencyToMicroseconds = 10.;
-        }
-    }
-    if (usePerformanceCounter) QueryPerformanceCounter(&t);
-    else {
-        GetSystemTimeAsFileTime(&f);
-        t.QuadPart = f.dwHighDateTime;
-        t.QuadPart <<= 32;
-        t.QuadPart |= f.dwLowDateTime;
-    }
-
-    t.QuadPart -= offset.QuadPart;
-    microseconds = (double)t.QuadPart / frequencyToMicroseconds;
-    t.QuadPart = microseconds;
-    tv->tv_sec = t.QuadPart / 1000000;
-    tv->tv_usec = t.QuadPart % 1000000;
-    return (0);
-}
-#endif
-
 int testMassiveInsert(int inserts) {
-    int interval;
-    #ifdef LINUX
-    timespec ts1;
-    timespec ts2;
-    interval = CLOCK_REALTIME;//CLOCK_PROCESS_CPUTIME_ID;
-    #endif
-    #ifdef WINDOWS
-    struct timeval ts1;
-    struct timeval ts2;
-    interval = 0;
-    #endif
+    std::auto_ptr<Logger> log(getLogger(NULL));
 
-    clock_gettime(interval, &ts1);
+    log->startTimeRecord();
+
     FileOutputStream fos("temp.txt", "wb+");
 
     for (int x = 0; x < inserts; x++) {
@@ -149,53 +50,30 @@ int testMassiveInsert(int inserts) {
             fos.writeString(obj->getString("_id"));
         }
         if ((x % 1000000) == 0) {
-            clock_gettime(interval, &ts2);
 
-            #ifdef LINUX
-            timespec etime = diff(ts1, ts2);
-            double secs = etime.tv_sec + ((double)etime.tv_nsec / 1000000000.0);
-            #else
-            struct timeval etime = diff(ts1, ts2);
-            double secs = etime.tv_sec + ((double)etime.tv_usec / 1000000.0);
-            #endif
-
-            cout<< "inserts " << x << ", secs: " << secs << endl;
-
-            cout << "Throughput: " << (x / secs) << " ops." << endl;
+            cout<< "inserts " << x << endl;
         }
     }
     fos.close();
 
-    clock_gettime(interval, &ts2);
+    log->stopTimeRecord();
 
-    #ifdef LINUX
-    timespec etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_nsec / 1000000000.0);
-    #else
-    struct timeval etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_usec / 1000000.0);
-    #endif
+    int secs = log->recordedTime().totalSecs();
 
     cout<< "inserts " << inserts << ", secs: " << secs << endl;
 
-    cout << "Throughput: " << (inserts / secs) << " ops." << endl;
-    cout << "------------------------------------------------------------" << endl;
+    if (secs > 0) {
+        cout << "Throughput: " << (inserts / secs) << " ops." << endl;
+        cout << "------------------------------------------------------------" << endl;
+    }
 }
 
 void testFinds() {
-   int interval;
-    #ifdef LINUX
-    timespec ts1;
-    timespec ts2;
-    interval = CLOCK_REALTIME;
-    #endif
-    #ifdef WINDOWS
-    struct timeval ts1;
-    struct timeval ts2;
-    interval = 0;
-    #endif
 
-    clock_gettime(interval, &ts1);
+
+    std::auto_ptr<Logger> log(getLogger(NULL));
+
+    log->startTimeRecord();
 
     for (std::vector<string*>::iterator i = __ids.begin(); i != __ids.end(); i++) {
         string* id = *i;
@@ -216,34 +94,21 @@ void testFinds() {
         delete(obj);
     }
 
-    clock_gettime(interval, &ts2);
+    log->stopTimeRecord();
 
-    #ifdef LINUX
-    timespec etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_nsec / 1000000000.0);
-    #else
-    struct timeval etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_usec / 1000000.0);
-    #endif
+    int secs = log->recordedTime().totalSecs();
 
     cout<< "finds " << __ids.size() << ", secs: " << secs << endl;
 
-    cout << "Throughput: " << (__ids.size() / secs) << " ops." << endl;
-    cout << "------------------------------------------------------------" << endl;
+    if (secs > 0) {
+        cout << "Throughput: " << (__ids.size() / secs) << " ops." << endl;
+        cout << "------------------------------------------------------------" << endl;
+    }
 }
 
 void testFindPrevious() {
-   int interval;
-    #ifdef LINUX
-    timespec ts1;
-    timespec ts2;
-    interval = CLOCK_REALTIME; //CLOCK_PROCESS_CPUTIME_ID;
-    #endif
-    #ifdef WINDOWS
-    struct timeval ts1;
-    struct timeval ts2;
-    interval = 0;
-    #endif
+
+    std::auto_ptr<Logger> log(getLogger(NULL));
 
     FileInputStream fis("temp.txt", "rb");
     std::vector<std::string*> ids;
@@ -252,7 +117,7 @@ void testFindPrevious() {
     }
     fis.close();
 
-    clock_gettime(interval, &ts1);
+    log->startTimeRecord();
 
     for (std::vector<string*>::iterator i = ids.begin(); i != ids.end(); i++) {
         string* id = *i;
@@ -273,20 +138,16 @@ void testFindPrevious() {
         delete(obj);
     }
 
-    clock_gettime(interval, &ts2);
+    log->stopTimeRecord();
 
-    #ifdef LINUX
-    timespec etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_nsec / 1000000000.0);
-    #else
-    struct timeval etime = diff(ts1, ts2);
-    double secs = etime.tv_sec + ((double)etime.tv_usec / 1000000.0);
-    #endif
+    int secs = log->recordedTime().totalSecs();
 
     cout<< "finds " << ids.size() << ", secs: " << secs << endl;
 
-    cout << "Throughput: " << (ids.size() / secs) << " ops." << endl;
-    cout << "------------------------------------------------------------" << endl;
+    if (secs > 0) {
+        cout << "Throughput: " << (ids.size() / secs) << " ops." << endl;
+        cout << "------------------------------------------------------------" << endl;
+    }
 }
 
 int main(int argc, char* args[])
