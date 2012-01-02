6df3fad3d4a86a045bcd98b034fc27e3ec5a57e1
Cross <crossleyjuan@gmail.com>
2012-01-01 22:03:22 -0500

    Demo

---
+++ db/demo/test.php
@@ -0,0 +1,35 @@
+<?php
+
+	session_start(); // start up your PHP session! 
+
+if (!extension_loaded('djonPhpExt')) {
+	if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+		dl('djonPhpExt.dll');
+	} else {
+		dl('djonPhpExt.so');
+	}
+}
+
+if (!extension_loaded('djonPhpExt')) {
+	echo "Cannot load extension";
+} else {
+   $c = NULL;
+	if (isset($_SESSION['connection'])) {
+		$c = $_SESSION['connection'];
+	}
+	if (is_null($c)) {
+		echo '<p>Starting new connection</p>';
+		$c = new Connection();
+		$_SESSION['connection'] = $c;
+
+	}
+
+	$c->djon_connect("localhost");
+	$guid = uniqid();
+	$json = "{ _id: '$guid', name: 'Juan', lastName: 'Cross'}";
+
+	$c->djon_insert('a', $json);
+
+	echo '<p>Inserted</p>';
+}
+?>
