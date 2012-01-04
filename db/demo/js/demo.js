df0e7b8e0f66d3ea56a80b82dfa4b743504d32de
Cross <crossleyjuan@gmail.com>
2012-01-03 23:31:50 -0500

    demo

--- db/demo/js/demo.js
+++ db/demo/js/demo.js
@@ -1,4 +1,14 @@
+function preparePost() {
+	var post = $("#post");
+
+	$("<label>Elige el tipo de anuncio</label>").appendTo(post);
+
+	var sel = $("<select></select>");
+	sel.appendTo(post);
+
+	$("<option value='1'>Venta de casa</option>").appendTo(sel);
+}
 
 $(document).ready(function() {
-		$
+		preparePost();
 });
