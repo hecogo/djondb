5d21a137f855859a946367e62a5302b036d34595
Cross <crossleyjuan@gmail.com>
2012-08-21 23:39:49 -0500

    fixed all the errors from lintian in the debian package

---
+++ db/debian/usr/include/djondb_0.1/defs.h
@@ -0,0 +1,25 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+#ifdef _WIN32
+#define WINDOWS
+#endif
+#ifdef __linux
+#define LINUX
+#endif
+
+#ifndef _WIN32
+#include "../config.h"
+#else
+#define VERSION "0.120120821"
+#define PACKAGE_VERSION "0.1"
+#endif
+
+#ifdef __MACH__ || __APPLE__ || TARGET_OS_MAC
+#define MAC
+#endif
+
+
+// #define TESTING
+
+#endif // DEFS_H_INCLUDED
---
+++ db/debian/usr/include/djondb_0.1/djondb_client.h
@@ -0,0 +1,10 @@
+#ifndef DJONDB_CLIENT_H_INCLUDED
+#define DJONDB_CLIENT_H_INCLUDED
+
+#include "bson.h"
+#include "util.h"
+#include "connection.h"
+#include "connectionmanager.h"
+
+
+#endif // DJONDB_CLIENT_H_INCLUDED
