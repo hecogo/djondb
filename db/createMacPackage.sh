25e0279114fb6840ffbf436bd2e224b52a651ccc
Juan Pablo Crossley <cross@djondb.com>
2012-11-18 23:12:34 -0500

    djondb pkg added

--- db/createMacPackage.sh
+++ db/createMacPackage.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 cd mac
-rm -rf djonpkg
-mkdir djonpkg
+rm djonpkg/usr/bin/*
+rm djonpkg/usr/lib/*
 cp -R ../obj/usr djonpkg/usr
 
 cd djonpkg
 rm usr/bin/test*
 rm usr/lib/*.a
 rm usr/lib/*.la
+rm -rf usr/include/djon-*
 cp /usr/lib/libant* usr/lib/
 cp /usr/lib/libv8* usr/lib/
